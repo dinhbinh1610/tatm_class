0b896dfcc4930b66812696ffc8c368b9e0b5ed4e
dinhbinh1610 <dinhbinh161@gmail.com>
2016-02-20 09:37:22 +0700

    last

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('*/image', express.static(path.join(__dirname, 'public/image')));
+app.use('*/js', express.static(path.join(__dirname, 'public/js')));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/center.js
+++ routes/center.js
@@ -9,8 +9,20 @@ router.get('/index', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('center/about', { title: 'Trang giới thiệu trung tâm' });
 });
+
 router.get('/about/edit', function(req, res, next) {
   res.render('center/edit-about', { title: 'Trang giới thiệu trung tâm' });
+});
+
+router.get('/list-classes', function(req, res, next) {
+  res.render('center/list_classes', { title: 'Trang danh sách lớp học' });
+});
+
+router.get('/list-teachers', function(req, res, next) {
+  res.render('center/list_teachers', { title: 'Trang danh sách giáo viên' });
+});
+router.get('/tuyensinh', function(req, res, next) {
+  res.render('center/tuyensinh', { title: 'Trang tuyển sinh' });
 });
 
 module.exports = router;
